5a4c245b7979af53a627c301657d8438272a740d
ktoyam <nikita.shatokhin_a@mail.ru>
2020-09-20 13:30:45 -0700

    added pract4->ex2

---
+++ src/pract4/ex2/Square.java
@@ -0,0 +1,40 @@
+package pract4.ex2;
+
+public class Square extends Rectangle{
+    public Square() {
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+
+    public double getSide(){
+        return this.length;
+    }
+    public void setSide(double side){
+        this.length = this.width = side;
+    }
+
+    @Override
+    public void setLength(double side) {
+        setSide(side);
+    }
+
+    @Override
+    public void setWidth(double side) {
+        setSide(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
